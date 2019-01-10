80943e0502c4c1c1c935e6dfb76e43ea4f9f45af
Naomi Seyfer <seyfer@google.com>
2019-01-09 17:21:08 -0800

    Get revision and service listing skeletons in.
    This adds the kn program, with basic commands for service and revision listing
    in.
    
    kn service list
    kn revision list
    
    They use the genericclioptions library from kubernetes to support jsonpath,
    yaml, and json output. The default is to just list the names of the objects, for
    now, until we add in some of the kubectl-based libraries for creating tables in
    another commit.
    
    This is deliberately bare-bones; I am looking to get the basic skeletons of how
    this repository is laid out down.
    
    No tests yet. Soon.

---
+++ cmd/kn/commands/service_list.go
@@ -0,0 +1,77 @@
+// Copyright © 2018 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	serving "github.com/knative/serving/pkg/client/clientset/versioned/typed/serving/v1alpha1"
+	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var serviceListPrintFlags *genericclioptions.PrintFlags
+
+// listCmd represents the list command
+var serviceListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List available services.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// use the current context in kubeconfig
+		config, err := clientcmd.BuildConfigFromFlags("", kubeCfgFile)
+		if err != nil {
+			panic(err.Error())
+		}
+		client, err := serving.NewForConfig(config)
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+		namespace := cmd.Flag("namespace").Value.String()
+		service, err := client.Services(namespace).List(v1.ListOptions{})
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		printer, err := serviceListPrintFlags.ToPrinter()
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+		service.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+			Group:   "knative.dev",
+			Version: "v1alpha1",
+			Kind:    "Service"})
+		err = printer.PrintObj(service, os.Stdout)
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	serviceCmd.AddCommand(serviceListCmd)
+
+	serviceListPrintFlags = genericclioptions.NewPrintFlags("").WithDefaultOutput(
+		"jsonpath={range .items[*]}{.metadata.name}{\"\\n\"}{end}")
+	serviceListPrintFlags.AddFlags(serviceListCmd)
+}
