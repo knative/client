38932b1d61e43d8d2ee6610466d77e8c3ee3be67
Naomi Seyfer <naomi@seyfer.org>
2019-01-22 09:03:58 -0800

    Get revision and service listing skeletons in. (#2)
    * Get revision and service listing skeletons in.
    
    This adds the kn program, with basic commands for service and revision listing
    in.
    
    kn service list
    kn revision list
    
    They use the genericclioptions library from kubernetes to support jsonpath,
    yaml, and json output. The default is to just list the names of the objects, for
    now, until we add in some of the kubectl-based libraries for creating tables in
    another commit.
    
    This is deliberately bare-bones; I am looking to get the basic skeletons of how
    this repository is laid out down.
    
    No tests yet. Soon.
    
    * Move commands to pkg, so cmd only contains minimalist main.go
    
    * RunE instead of Run. The defaults on Cobra command generation are weird.
    
    * Oops forgot to change types of stuff
    
    * Do not panic on bad config file
    
    * Make commands no longer global
    
    * Make configuration initialization not static in the module, but rather triggered by main.go init

---
+++ pkg/kn/commands/revision_list.go
@@ -0,0 +1,71 @@
+// Copyright © 2018 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commands
+
+import (
+	"os"
+
+	serving "github.com/knative/serving/pkg/client/clientset/versioned/typed/serving/v1alpha1"
+	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var revisionListPrintFlags *genericclioptions.PrintFlags
+
+// listCmd represents the list command
+func NewRevisionListCommand() *cobra.Command {
+	revisionListPrintFlags = genericclioptions.NewPrintFlags("").WithDefaultOutput(
+		"jsonpath={range .items[*]}{.metadata.name}{\"\\n\"}{end}")
+	revisionListCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List available revisions.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// use the current context in kubeconfig
+			config, err := clientcmd.BuildConfigFromFlags("", kubeCfgFile)
+			if err != nil {
+				return err
+			}
+			client, err := serving.NewForConfig(config)
+			if err != nil {
+				return err
+			}
+			namespace := cmd.Flag("namespace").Value.String()
+			revision, err := client.Revisions(namespace).List(v1.ListOptions{})
+			if err != nil {
+				return err
+			}
+
+			printer, err := revisionListPrintFlags.ToPrinter()
+			if err != nil {
+				return err
+			}
+			revision.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+				Group:   "knative.dev",
+				Version: "v1alpha1",
+				Kind:    "Revision"})
+			err = printer.PrintObj(revision, os.Stdout)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	revisionListPrintFlags.AddFlags(revisionListCmd)
+	return revisionListCmd
+}
---
+++ pkg/kn/commands/root.go
@@ -0,0 +1,89 @@
+// Copyright © 2018 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commands
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+var kubeCfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+func NewKnCommand() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "kn",
+		Short: "Knative client.",
+		Long: `Manage your Knative building blokcs:
+
+Serving: Manage your services and release new software to them.
+Build: Create builds and keep track of their results.
+Eventing: Manage event subscriptions and channels. Connect up event sources.`,
+	}
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kn.yaml)")
+	rootCmd.PersistentFlags().StringVar(&kubeCfgFile, "kubeconfig", "", "kubectl config file (default is $HOME/.kube/config)")
+	rootCmd.AddCommand(NewServiceCommand())
+	rootCmd.AddCommand(NewRevisionCommand())
+	return rootCmd
+}
+
+func InitializeConfig() {
+	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initKubeConfig)
+
+}
+
+func initKubeConfig() {
+	if kubeCfgFile == "" {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		kubeCfgFile = filepath.Join(home, ".kube", "config")
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".kn" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".kn")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
