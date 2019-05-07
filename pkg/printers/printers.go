06f8cd03c77c488997d44abf9bb4c33ea0787f4b
Navid Shaikh <nshaikh@redhat.com>
2019-05-07 16:01:47 +0530

    Adds licenses
     and references the original implementation file link.
    
     Removes pkg/printers/printers.go

--- pkg/printers/printers.go
+++
@@ -1,7 +0,0 @@
-package printers
-
-// AddHandlers adds print handlers for knative resources
-func AddHandlers(h PrintHandler) {
-	// resources listing table definition goes here
-	// table rows population logic goes here too
-}
