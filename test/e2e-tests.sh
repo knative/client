c60ba71f9ce2dc9f46136030aa39c834ff3136a3
Navid Shaikh <nshaikh@redhat.com>
2019-05-16 19:54:54 +0530

    Adds the service update command back to e2e tests
     also increases the sleep time between create and get

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -45,14 +45,15 @@ initialize $@
 header "Running tests"
 
 ./kn service create hello --image gcr.io/knative-samples/helloworld-go -e TARGET=Knative || fail_test
-sleep 2
+sleep 5
 ./kn service get || fail_test
-#./kn service update hello --env TARGET=kn || fail_test
+./kn service update hello --env TARGET=kn || fail_test
+sleep 3
 ./kn revision get || fail_test
-#./kn service get || fail_test
+./kn service get || fail_test
 ./kn service create hello --force --image gcr.io/knative-samples/helloworld-go -e TARGET=Awesome || fail_test
 ./kn service create foo --force --image gcr.io/knative-samples/helloworld-go -e TARGET=foo || fail_test
-sleep 2
+sleep 5
 ./kn revision get || fail_test
 ./kn service get || fail_test
 ./kn service describe hello || fail_test
