c2d9b969eef32a61e848d7727856fc0d1e0b14af
Navid Shaikh <nshaikh@redhat.com>
2019-06-26 16:06:24 +0530

    Adds integration tests for revision delete command
     Added revision delete command tests in new workflow namely
     TestRevisionWorkflow.

---
+++ test/e2e/revision_workflow_test.go
@@ -0,0 +1,46 @@
+// Copyright 2019 The Knative Authors
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or im
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build e2e
+
+package e2e
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRevisionWorkflow(t *testing.T) {
+	teardown := Setup(t)
+	defer teardown(t)
+
+	testServiceCreate(t, k, "hello")
+	testDeleteRevision(t, k, "hello")
+	testServiceDelete(t, k, "hello")
+}
+
+func testDeleteRevision(t *testing.T, k kn, serviceName string) {
+	revName, err := k.RunWithOpts([]string{"revision", "list", "-o=jsonpath={.items[0].metadata.name}"}, runOpts{})
+	if err != nil {
+		t.Errorf("Error executing 'revision list -o' command. Error: %s", err.Error())
+	}
+	out, err := k.RunWithOpts([]string{"revision", "delete", revName}, runOpts{})
+	if err != nil {
+		t.Errorf("Error executing 'revision delete %s' command. Error: %s", revName, err.Error())
+	}
+	expectedOutput := fmt.Sprintf("Revision '%s' successfully deleted in namespace '%s'.\n", revName, defaultKnE2ENamespace)
+	if out != expectedOutput {
+		t.Errorf("Wrong output from 'revision delete %s' command. Actual output:\n%s\nExpected output:\n%s\n", revName, out, expectedOutput)
+	}
+}
