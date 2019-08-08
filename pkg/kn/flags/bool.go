8591ac437dc175fd2be3fab8f1d168b123f9875d
Naomi Seyfer <seyfer@google.com>
2019-08-07 22:58:45 -0700

    Godoc for new functions

--- pkg/kn/flags/bool.go
+++ pkg/kn/flags/bool.go
@@ -24,6 +24,9 @@ import (
 
 var negPrefix = "no-"
 
+// AddBothBoolFlags adds the given flag in both `--foo` and `--no-foo` variants.
+// If you do this, make sure you call ReconcileBoolFlags later to catch errors and
+// set the relationship between the flag values.
 func AddBothBoolFlags(f *pflag.FlagSet, p *bool, name, short string, value bool, usage string) {
 
 	negativeName := negPrefix + name
@@ -44,6 +47,8 @@ func AddBothBoolFlags(f *pflag.FlagSet, p *bool, name, short string, value bool,
 	}
 }
 
+// ReconcileBoolFlags sets the value of the all the "--foo" flags based on
+// "--no-foo" if provided, and returns an error if both were provided.
 func ReconcileBoolFlags(f *pflag.FlagSet) error {
 	var err error
 	f.Visit(func(flag *pflag.Flag) {
