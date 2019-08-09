74b3e10f761a414bb3285d66a35787677c5acac9
Doug Davis <dug@us.ibm.com>
2019-08-09 10:01:05 -0400

    Add support for -A variant of --all-namespaces (#356)
    * Add support for -A variant of --all-namespaces
    
    -A is my newest favorite kubectl flag, it's so much easier/faster to type
    than --all-namespaces. kn users should benefit from it too!
    
    Signed-off-by: Doug Davis <dug@us.ibm.com>
    
    * tweak tests
    
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- pkg/kn/commands/namespaced_test.go
+++ pkg/kn/commands/namespaced_test.go
@@ -68,16 +68,21 @@ func TestGetNamespaceAllNamespacesSet(t *testing.T) {
 	testCmd := testCommandGenerator(true)
 	expectedNamespace := ""
 	sampleNamespace := "test1"
-	testCmd.SetArgs([]string{"--namespace", sampleNamespace, "--all-namespaces"})
-	testCmd.Execute()
-	kp := &KnParams{fixedCurrentNamespace: FakeNamespace}
-	actualNamespace, err := kp.GetNamespace(testCmd)
-	if err != nil {
-		t.Fatal(err)
+
+	// Test both variants of the "all namespaces" flag
+	for _, arg := range []string{"--all-namespaces", "-A"} {
+		testCmd.SetArgs([]string{"--namespace", sampleNamespace, arg})
+		testCmd.Execute()
+		kp := &KnParams{fixedCurrentNamespace: FakeNamespace}
+		actualNamespace, err := kp.GetNamespace(testCmd)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if actualNamespace != expectedNamespace {
+			t.Fatalf("Incorrect namespace retrieved: %v, expected: %v", actualNamespace, expectedNamespace)
+		}
 	}
-	if actualNamespace != expectedNamespace {
-		t.Fatalf("Incorrect namespace retrieved: %v, expected: %v", actualNamespace, expectedNamespace)
-	}
+
 }
 
 // test with all-namespace flag set without any namespace flag set
@@ -85,16 +90,21 @@ func TestGetNamespaceAllNamespacesSet(t *testing.T) {
 func TestGetNamespaceDefaultAllNamespacesUnset(t *testing.T) {
 	testCmd := testCommandGenerator(true)
 	expectedNamespace := ""
-	testCmd.SetArgs([]string{"--all-namespaces"})
-	testCmd.Execute()
-	kp := &KnParams{fixedCurrentNamespace: FakeNamespace}
-	actualNamespace, err := kp.GetNamespace(testCmd)
-	if err != nil {
-		t.Fatal(err)
+
+	// Test both variants of the "all namespaces" flag
+	for _, arg := range []string{"--all-namespaces", "-A"} {
+		testCmd.SetArgs([]string{arg})
+		testCmd.Execute()
+		kp := &KnParams{fixedCurrentNamespace: FakeNamespace}
+		actualNamespace, err := kp.GetNamespace(testCmd)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if actualNamespace != expectedNamespace {
+			t.Fatalf("Incorrect namespace retrieved: %v, expected: %v", actualNamespace, expectedNamespace)
+		}
 	}
-	if actualNamespace != expectedNamespace {
-		t.Fatalf("Incorrect namespace retrieved: %v, expected: %v", actualNamespace, expectedNamespace)
-	}
+
 }
 
 // test with all-namespaces flag not defined for command
