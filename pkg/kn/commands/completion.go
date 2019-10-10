9e452732374e8de0a450943c9433ca7df6a5635d
Michael Maximilien <maxim@us.ibm.com>
2019-10-10 15:20:38 -0700

    removes zsh completion since it does not work (fixes #426)

--- pkg/kn/commands/completion.go
+++ pkg/kn/commands/completion.go
@@ -20,26 +20,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type CompletionFlags struct {
-	Zsh bool
-}
-
 func NewCompletionCommand(p *KnParams) *cobra.Command {
-	var completionFlags CompletionFlags
-
-	completionCmd := &cobra.Command{
+	return &cobra.Command{
 		Use:    "completion",
-		Short:  "Output shell completion code (default Bash)",
-		Hidden: true, // Don't show this in help listing.
+		Short:  "Output shell completion code (Bash)",
+		Hidden: true, // Don't show this in help listing
 		Run: func(cmd *cobra.Command, args []string) {
-			if completionFlags.Zsh {
-				cmd.Root().GenZshCompletion(os.Stdout)
-			} else {
-				cmd.Root().GenBashCompletion(os.Stdout)
-			}
+			cmd.Root().GenBashCompletion(os.Stdout)
 		},
 	}
-
-	completionCmd.Flags().BoolVar(&completionFlags.Zsh, "zsh", false, "Generates completion code for Zsh shell.")
-	return completionCmd
 }
