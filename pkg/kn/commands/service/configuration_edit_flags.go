8f6a436b48fd362cceccfd4e36a04146a545a24a
itsmurugappan <itsmurugappan@gmail.com>
2020-02-19 05:43:38 -0800

    review comments  for pr 679

--- pkg/kn/commands/service/configuration_edit_flags.go
+++ pkg/kn/commands/service/configuration_edit_flags.go
@@ -190,7 +190,7 @@ func (p *ConfigurationEditFlags) addSharedFlags(command *cobra.Command) {
 		"",
 		"Image pull secret to set. An empty argument (\"\") clears the pull secret. The referenced secret must exist in the service's namespace.")
 	p.markFlagMakesRevision("pull-secret")
-	command.Flags().Int64VarP(&p.RunAsUser, "run-as-user", "", 0, "The user to run the container (e.g., 1001).")
+	command.Flags().Int64VarP(&p.RunAsUser, "run-as-user", "", 0, "The user ID to run the container (e.g., 1001).")
 	p.markFlagMakesRevision("run-as-user")
 }
 
