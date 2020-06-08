47eff2682fc8a86f2fb0510550649c7a7a8114a3
Dave Protasowski <dprotaso@gmail.com>
2020-06-08 15:31:26 -0400

    inline go_run_tool

--- hack/build.sh
+++ hack/build.sh
@@ -16,8 +16,6 @@
 
 set -o pipefail
 
-source $(dirname $0)/../scripts/test-infra/library.sh
-
 source_dirs="cmd pkg test lib"
 
 # Store for later
@@ -111,6 +109,34 @@ go_fmt() {
   echo "🧹 ${S}Format"
   find $(echo $source_dirs) -name "*.go" -print0 | xargs -0 gofmt -s -w
 }
+
+# Run a go tool, installing it first if necessary.
+# Parameters: $1 - tool package/dir for go get/install.
+#             $2 - tool to run.
+#             $3..$n - parameters passed to the tool.
+run_go_tool() {
+  local tool=$2
+  local install_failed=0
+  if [[ -z "$(which ${tool})" ]]; then
+    local action=get
+    [[ $1 =~ ^[\./].* ]] && action=install
+    # Avoid running `go get` from root dir of the repository, as it can change go.sum and go.mod files.
+    # See discussions in https://github.com/golang/go/issues/27643.
+    if [[ ${action} == "get" && $(pwd) == "${REPO_ROOT_DIR}" ]]; then
+      local temp_dir="$(mktemp -d)"
+      # Swallow the output as we are returning the stdout in the end.
+      pushd "${temp_dir}" > /dev/null 2>&1
+      GOFLAGS="" go ${action} "$1" || install_failed=1
+      popd > /dev/null 2>&1
+    else
+      GOFLAGS="" go ${action} "$1" || install_failed=1
+    fi
+  fi
+  (( install_failed )) && return ${install_failed}
+  shift 2
+  ${tool} "$@"
+}
+
 
 source_format() {
   set +e
