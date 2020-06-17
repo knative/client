98f46e5bee867f0ca1f370ddf6d180fdc66cc51d
Roland Huß <roland@ro14nd.de>
2020-06-17 14:51:11 +0200

    Update pkg/kn/commands/source/apiserver/delete.go
    Co-authored-by: Navid Shaikh <nshaikh@redhat.com>

--- pkg/kn/commands/source/apiserver/delete.go
+++ pkg/kn/commands/source/apiserver/delete.go
@@ -27,7 +27,7 @@ import (
 func NewAPIServerDeleteCommand(p *commands.KnParams) *cobra.Command {
 	deleteCommand := &cobra.Command{
 		Use:   "delete NAME",
-		Short: "Delete api-server sources",
+		Short: "Delete an api-server source",
 		Example: `
   # Delete an ApiServerSource 'k8sevents' in default namespace
   kn source apiserver delete k8sevents`,
