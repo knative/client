ca48f45757e2a067b56a5ecf7568db38fef7639c
Navid Shaikh <nshaikh@redhat.com>
2020-07-09 14:56:39 +0530

    Fix typo in YAML URL and add subheader for sugar-controller install

--- test/common.sh
+++ test/common.sh
@@ -37,6 +37,7 @@ function knative_setup() {
   if [ "${eventing_version}" = "latest" ]; then
     start_latest_knative_eventing
 
+    subheader "Installing eventing extension: sugar-controller (${eventing_version})"
     # install the sugar controller
     kubectl apply --filename https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-sugar-controller.yaml
     wait_until_pods_running knative-eventing || return 1
@@ -44,8 +45,9 @@ function knative_setup() {
   else
     start_release_knative_eventing "${eventing_version}"
 
+    subheader "Installing eventing extension: sugar-controller (${eventing_version})"
     # install the sugar controller
-    kubectl apply --filename https://storage.googleapis.com/knative-releases/eventing/previous/${eventing_version}/eventing-sugar-controller.yaml
+    kubectl apply --filename https://storage.googleapis.com/knative-releases/eventing/previous/v${eventing_version}/eventing-sugar-controller.yaml
     wait_until_pods_running knative-eventing || return 1
   fi
 }
