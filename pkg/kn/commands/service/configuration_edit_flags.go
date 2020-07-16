ba133ed43c5a953dc30df423561b570ca70d848c
David Simansky <dsimansk@redhat.com>
2020-07-16 21:38:37 +0200

    fix: Fix spelling in usage message

--- pkg/kn/commands/service/configuration_edit_flags.go
+++ pkg/kn/commands/service/configuration_edit_flags.go
@@ -279,7 +279,7 @@ func (p *ConfigurationEditFlags) AddCreateFlags(command *cobra.Command) {
 	command.Flags().BoolVar(&p.ForceCreate, "force", false,
 		"Create service forcefully, replaces existing service if any.")
 	command.Flags().StringVarP(&p.Filename, "filename", "f", "", "Create a service from file. "+
-		"The created service can be futher modified by combining with other options."+
+		"The created service can be further modified by combining with other options."+
 		"Example: -f /path/to/file --env NAME=value will also add environment variable.")
 	command.MarkFlagFilename("filename")
 	p.markFlagMakesRevision("filename")
