879e5bd35603c145962ae2e98cf949675824e393
David Simansky <dsimansk@redhat.com>
2020-07-21 15:05:35 +0200

    feat: Add support to combine service create --filename with other options (#937)
    * feat: Add support to combine service create --filename with other options
    
    * fix: Fix spelling in usage message
    
    * fix: Fix usage message
    
    * fix: Add test to cover errors
    
    * fix: Usage message wording
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * fix: Update codegen
    
    * chore: Add changelog entry
    
    * fix: Fix changelog formatting
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>

--- pkg/kn/commands/service/configuration_edit_flags.go
+++ pkg/kn/commands/service/configuration_edit_flags.go
@@ -278,8 +278,11 @@ func (p *ConfigurationEditFlags) AddCreateFlags(command *cobra.Command) {
 	p.addSharedFlags(command)
 	command.Flags().BoolVar(&p.ForceCreate, "force", false,
 		"Create service forcefully, replaces existing service if any.")
-	command.Flags().StringVarP(&p.Filename, "filename", "f", "", "Create a service from file.")
+	command.Flags().StringVarP(&p.Filename, "filename", "f", "", "Create a service from file. "+
+		"The created service can be further modified by combining with other options. "+
+		"For example, -f /path/to/file --env NAME=value adds also an environment variable.")
 	command.MarkFlagFilename("filename")
+	p.markFlagMakesRevision("filename")
 }
 
 // Apply mutates the given service according to the flags in the command.
