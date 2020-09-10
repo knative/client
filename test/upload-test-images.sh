bdca895d08740ccb6ce7c13d0fd02007834584e6
itsmurugappan <itsmurugappan@gmail.com>
2020-09-10 12:05:47 -0700

    add symlink for grpc example

--- test/upload-test-images.sh
+++ test/upload-test-images.sh
@@ -33,9 +33,6 @@ function upload_test_images() {
   # so the resulting yaml produced is ignored.
   # We limit the number of concurrent builds (jobs) to avoid OOMs.
   ko resolve --jobs=4 ${tag_option} -RBf "${image_dir}" > /dev/null
-
-  #build and publish images from vendor directory
-  ko resolve --jobs=4 ${tag_option} -RBf "${vendor_image_dir}" > /dev/null
 }
 
 : ${KO_DOCKER_REPO:?"You must set 'KO_DOCKER_REPO'"}
