1dd396fa30a690ef07d53eee1002a71227ae7826
Navid Shaikh <nshaikh@redhat.com>
2020-11-05 21:08:34 +0530

    Fix e2e tests

--- test/e2e/sink_test.go
+++ test/e2e/sink_test.go
@@ -50,11 +50,11 @@ func TestSink(t *testing.T) {
 	assert.Equal(t, out, "default")
 
 	// create a channel
-	err = test.ChannelCreate(r, "pipe")
+	test.ChannelCreate(r, "pipe")
 
 	t.Log("Update Ping source with a sink to the channel")
 	pingSourceUpdateSink(r, "testpingsource0", "channel:pipe")
-	out, err := test.GetResourceFieldsWithJSONPath(t, it, "pingsource", "testpingsource0", jpSinkRefNameInSpec)
+	out, err = test.GetResourceFieldsWithJSONPath(t, it, "pingsource", "testpingsource0", jpSinkRefNameInSpec)
 	assert.NilError(t, err)
 	assert.Equal(t, out, "pipe")
 
