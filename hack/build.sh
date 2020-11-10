c0ec89e55f13a210291a19fcc250d91b73c857e2
Roland Huß <roland@ro14nd.de>
2020-11-10 12:08:14 +0100

    feature: Add Linux arm64 builds to the release
    Fixes https://github.com/knative/client/issues/1101

--- hack/build.sh
+++ hack/build.sh
@@ -276,6 +276,8 @@ cross_build() {
   export CGO_ENABLED=0
   echo "   🐧 kn-linux-amd64"
   GOOS=linux GOARCH=amd64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-linux-amd64 ./cmd/... || failed=1
+  echo "   💪 kn-linux-arm64"
+  GOOS=linux GOARCH=arm64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-linux-arm64 ./cmd/... || failed=1
   echo "   🍏 kn-darwin-amd64"
   GOOS=darwin GOARCH=amd64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-darwin-amd64 ./cmd/... || failed=1
   echo "   🎠 kn-windows-amd64.exe"
--- hack/release.sh
+++ hack/release.sh
@@ -29,15 +29,17 @@ function build_release() {
 
   export GO111MODULE=on
   export CGO_ENABLED=0
-  echo "🚧 🐧 Building for Linux"
+  echo "🚧 🐧 Building for Linux (amd64)"
   GOOS=linux GOARCH=amd64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-linux-amd64 ./cmd/...
+  echo "🚧 💪 Building for Linux (arm64)"
+  GOOS=linux GOARCH=arm64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-linux-arm64 ./cmd/...
   echo "🚧 🍏 Building for macOS"
   GOOS=darwin GOARCH=amd64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-darwin-amd64 ./cmd/...
   echo "🚧 🎠 Building for Windows"
   GOOS=windows GOARCH=amd64 go build -mod=vendor -ldflags "${ld_flags}" -o ./kn-windows-amd64.exe ./cmd/...
   echo "🚧 🐳 Building the container image"
   ko resolve --strict ${KO_FLAGS} -f config/ > kn-image-location.yaml
-  ARTIFACTS_TO_PUBLISH="kn-darwin-amd64 kn-linux-amd64 kn-windows-amd64.exe kn-image-location.yaml"
+  ARTIFACTS_TO_PUBLISH="kn-darwin-amd64 kn-linux-amd64 kn-linux-arm64 kn-windows-amd64.exe kn-image-location.yaml"
   if type sha256sum >/dev/null 2>&1; then
     echo "🧮     Checksum:"
     sha256sum ${ARTIFACTS_TO_PUBLISH}
