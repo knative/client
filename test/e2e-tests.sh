268776d917d57d141e898a8daf2332cdb3b75ea4
Martin Gencur <mgencur@redhat.com>
2021-05-10 11:56:18 +0200

    Revert "TMP: Fix running in pre-submit"
    This reverts commit 35694f067e20e3be3d6367afb3cb97fed027759f.

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -36,22 +36,23 @@ source $(dirname $0)/common.sh
 export PATH=$PATH:${REPO_ROOT_DIR}
 
 run() {
-  # Create cluster
-  initialize $@ --skip-istio-addon
-
-  # Smoke test
-  eval smoke_test || fail_test
-
-  # Integration test
-  eval integration_test || fail_test
+  $(dirname $0)/tekton-tests.sh
+#  # Create cluster
+#  initialize $@ --skip-istio-addon
+#
+#  # Smoke test
+#  eval smoke_test || fail_test
+#
+#  # Integration test
+#  eval integration_test || fail_test
 
   success
 }
 
 integration_test() {
   header "Running tests for Knative Serving $KNATIVE_SERVING_VERSION and Eventing $KNATIVE_EVENTING_VERSION"
-  $(dirname $0)/tekton-tests.sh
-  #go_test_e2e -timeout=45m ./test/e2e || fail_test
+
+  go_test_e2e -timeout=45m ./test/e2e || fail_test
 }
 
 smoke_test_clean_up(){
