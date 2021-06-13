d537558822f3b0b403421e5717840564eb15912d
itsmurugappan <murugappan_chetty_sevugan@optum.com>
2021-06-12 20:19:58 -0700

    add ut for api server source

--- pkg/kn/commands/source/apiserver/update_test.go
+++ pkg/kn/commands/source/apiserver/update_test.go
@@ -15,6 +15,7 @@
 package apiserver
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -43,7 +44,7 @@ func TestApiServerSourceUpdate(t *testing.T) {
 	updated := createAPIServerSource("testsource", "Event", "v1", "testsa2", "Reference", map[string]string{"foo": "baz"}, createSinkv1("svc2", "default"))
 	apiServerRecorder.UpdateAPIServerSource(updated, nil)
 
-	output, err := executeAPIServerSourceCommand(apiServerClient, dynamicClient, "update", "testsource", "--service-account", "testsa2", "--sink", "ksvc:svc2", "--ce-override", "bla-", "--ce-override", "foo=baz")
+	output, err := executeAPIServerSourceCommand(apiServerClient, dynamicClient, "update", "testsource", "--service-account", "testsa2", "--sink", "ksvc:svc2", "--ce-override", "bla-", "--ce-override", "foo=baz", "--mode", "Reference")
 	assert.NilError(t, err)
 	assert.Assert(t, util.ContainsAll(output, "testsource", "updated", "default"))
 
@@ -62,4 +63,29 @@ func TestApiServerSourceUpdateDeletionTimestampNotNil(t *testing.T) {
 	assert.ErrorContains(t, err, present.Name)
 	assert.ErrorContains(t, err, "deletion")
 	assert.ErrorContains(t, err, "apiserver")
+}
+
+func TestApiServerUpdateError(t *testing.T) {
+	apiServerClient := v1.NewMockKnAPIServerSourceClient(t)
+	apiServerRecorder := apiServerClient.Recorder()
+
+	//check for name
+	argMissingMsg := "requires the name of the source as single argument"
+	apiServerRecorder.GetAPIServerSource("", nil, errors.New(argMissingMsg))
+	_, err := executeAPIServerSourceCommand(apiServerClient, nil, "update", "", "--resource", "Event:v1:key1=value1", "--service-account", "testsa", "--sink", "ksvc:testsvc", "--mode", "Reference")
+	assert.Error(t, err, argMissingMsg)
+
+	//check if sink is missing
+	dynamicClient := dynamicfake.CreateFakeKnDynamicClient("default", &servingv1.Service{
+		TypeMeta:   metav1.TypeMeta{Kind: "Service", APIVersion: "serving.knative.dev/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "svc2", Namespace: "default"},
+	})
+
+	present := createAPIServerSource("testsource", "Event", "v1", "testsa1", "Reference", map[string]string{"bla": "blub", "foo": "bar"}, createSinkv1("svc1", "default"))
+	apiServerRecorder.GetAPIServerSource("testsource", present, nil)
+	sinkMissingMsg := "services.serving.knative.dev \"svc3\" not found"
+	apiServerRecorder.UpdateAPIServerSource("", errors.New(sinkMissingMsg))
+
+	_, err = executeAPIServerSourceCommand(apiServerClient, dynamicClient, "update", "testsource", "--service-account", "testsa2", "--sink", "ksvc:svc3", "--ce-override", "bla-", "--ce-override", "foo=baz")
+	assert.Error(t, err, sinkMissingMsg)
 }
