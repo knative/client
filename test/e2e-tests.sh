d35c009217fd8e301d429d1b91ee7adb782d8693
Martin Gencur <mgencur@redhat.com>
2021-08-30 14:56:14 +0200

    Revert "TMP: Test Tekton in presubmits"
    This reverts commit 8996ac7692fff4e2b90ad6db657e528b7745a445.

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -51,8 +51,7 @@ run() {
 integration_test() {
   header "Running tests for Knative Serving $KNATIVE_SERVING_VERSION and Eventing $KNATIVE_EVENTING_VERSION"
 
-  bash ./test/tekton-tests.sh
-  #go_test_e2e -timeout=45m ./test/e2e || fail_test
+  go_test_e2e -timeout=45m ./test/e2e || fail_test
 }
 
 smoke_test_clean_up(){
