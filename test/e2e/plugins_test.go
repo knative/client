cfac4aee95dd7dedbc88ff9d0613f4a90d9e3fb0
David Simansky <dsimansk@redhat.com>
2021-10-18 16:39:55 +0200

    Update actions

--- test/e2e/plugins_test.go
+++ test/e2e/plugins_test.go
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build e2e
-// +build !eventing
-// +build !serving
+//go:build e2e && !eventing && !serving
+// +build e2e,!eventing,!serving
 
 package e2e
 
