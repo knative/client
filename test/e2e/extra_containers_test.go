a736af7129a14e852215c1928ccbea30c9d98619
David Simansky <dsimansk@redhat.com>
2021-10-18 17:56:36 +0200

    Combine update deps actions format manually (#1484)
    * upgrade to latest dependencies
    
    bumping knative.dev/eventing ff4bc40...5f69768:
      > 5f69768 upgrade to latest dependencies (# 5817)
      > a6a819d upgrade to latest dependencies (# 5812)
    bumping knative.dev/networking c2191da...91e7d04:
      > 91e7d04 upgrade to latest dependencies (# 531)
      > 16ef524 upgrade to latest dependencies (# 529)
      > cc26c45 Update actions (# 530)
      > 0c6b765 Deprecate httpProtocol: "Disabled" in config-network (# 528)
    bumping knative.dev/pkg fa2f8f1...3463dd7:
      > 3463dd7 upgrade to latest dependencies (# 2315)
      > a5bb759 upgrade to latest dependencies (# 2314)
      > e61a20d Update actions (# 2313)
      > 0e24b49 Format go code (# 2312)
    bumping knative.dev/hack 815cd31...47ac3b0:
      > 47ac3b0 Split build tags that are in a comma separated list (# 83)
      > 8687668 [# 50] Add error message when Bash < 4 (# 79)
      > 8d79600 Fixes # 81 (# 82)
      > 51582ce fix misspelling functionallity -> functionality (# 78)
      > 675e1fd Correctly specify the user when creating clusterrolebinding for kind (# 80)
    bumping knative.dev/serving 7c6d4bc...30087cd:
      > 30087cd upgrade to latest dependencies (# 12145)
      > e5d8560 Format go code (# 12139)
      > c9b2e25 upgrade to latest dependencies (# 12141)
      > ebdd09e Update actions (# 12140)
      > caced61 Update net-certmanager nightly (# 12138)
      > c6f1707 Add logic to retry pause/resume for pod freezer (# 12049)
      > 0780c10 Trust k8s readiness signal (# 12086)
      > 0592224 Update net-kourier nightly (# 12135)
      > 1cac17a Fix HTTP Redirect test for Contour (# 12124)
      > 8ecc7f9 Update net-istio nightly (# 12134)
      > f37a630 Update net-certmanager nightly (# 12133)
      > 04efc1e Update net-kourier nightly (# 12128)
      > 8cc9b0d upgrade to latest dependencies (# 12132)
    
    * Update actions

--- test/e2e/extra_containers_test.go
+++ test/e2e/extra_containers_test.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build e2e
-// +build !serving
+//go:build e2e && !serving
+// +build e2e,!serving
 
 package e2e
 
