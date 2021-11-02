a1cf5bf81f77b9602607735af286fb3cefb3fee2
David Simansky <dsimansk@redhat.com>
2021-11-02 19:39:41 +0100

    Update Serving and Eventing versions in E2E tests (#1507)

--- test/presubmit-integration-tests-latest-release.sh
+++ test/presubmit-integration-tests-latest-release.sh
@@ -17,7 +17,7 @@
 # This script is used in Knative/test-infra as a custom prow job to run the
 # integration tests against Knative Serving / Eventing of a specific version.
 
-export KNATIVE_SERVING_VERSION="0.26.0"
-export KNATIVE_EVENTING_VERSION="0.26.0"
+export KNATIVE_SERVING_VERSION="1.0.0"
+export KNATIVE_EVENTING_VERSION="1.0.0"
 
 $(dirname $0)/presubmit-tests.sh --integration-tests
