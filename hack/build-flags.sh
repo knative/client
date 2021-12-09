c0e82806c0c5e92d881501127bf337c47fa06cf7
Chris Suszyński <ksuszyns@redhat.com>
2021-12-09 17:19:36 +0100

    Allow overriding of build.sh for downstream projects

--- hack/build-flags.sh
+++ hack/build-flags.sh
@@ -14,20 +14,30 @@
 
 function build_flags() {
   local base="${1}"
-  local now="$(date -u '+%Y-%m-%d %H:%M:%S')"
-  local rev="$(git rev-parse --short HEAD)"
+  local now rev
+  now="$(date -u '+%Y-%m-%d %H:%M:%S')"
+  rev="$(git rev-parse --short HEAD)"
   local pkg="knative.dev/client/pkg/kn/commands/version"
   local version="${TAG:-}"
   # Use vYYYYMMDD-local-<hash> for the version string, if not passed.
   if [[ -z "${version}" ]]; then
     # Get the commit, excluding any tags but keeping the "dirty" flag
-    local commit="$(git describe --always --dirty --match '^$')"
+    local commit
+    commit="$(git describe --always --dirty --match '^$')"
     [[ -n "${commit}" ]] || abort "error getting the current commit"
     version="v$(date +%Y%m%d)-local-${commit}"
   fi
   # Extract Eventing and Serving versions from go.mod
-  local version_serving=$(cat go.mod | grep "knative.dev/serving " | sed -s 's/.* \(v.[\.0-9]*\).*/\1/')
-  local version_eventing=$(cat go.mod | grep "knative.dev/eventing " | sed -s 's/.* \(v.[\.0-9]*\).*/\1/')
+  local version_serving version_eventing
+  version_serving=$(grep "knative.dev/serving " "${base}/go.mod" \
+    | sed -s 's/.* \(v.[\.0-9]*\).*/\1/')
+  version_eventing=$(grep "knative.dev/eventing " "${base}/go.mod" \
+    | sed -s 's/.* \(v.[\.0-9]*\).*/\1/')
 
-  echo "-X '${pkg}.BuildDate=${now}' -X ${pkg}.Version=${version} -X ${pkg}.GitRevision=${rev} -X ${pkg}.VersionServing=${version_serving} -X ${pkg}.VersionEventing=${version_eventing}"
+  echo "-X '${pkg}.BuildDate=${now}' \
+  -X ${pkg}.Version=${version} \
+  -X ${pkg}.GitRevision=${rev} \
+  -X ${pkg}.VersionServing=${version_serving} \
+  -X ${pkg}.VersionEventing=${version_eventing}\
+  ${EXTERNAL_LD_FLAGS:-}"
 }
