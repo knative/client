fc85cff55cd0d458e438bd8bf9643e042934ec7d
David Simansky <dsimansk@redhat.com>
2022-01-21 10:31:05 +0100

    Remove k8s cluster version pin (#1572)

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -37,7 +37,7 @@ export PATH=$PATH:${REPO_ROOT_DIR}
 
 run() {
   # Create cluster
-  initialize $@ --skip-istio-addon --cluster-version=1.20
+  initialize $@ --skip-istio-addon
 
   # Smoke test
   eval smoke_test || fail_test
