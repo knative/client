1cfe75081ab7039526d60967acd0d3e4a90a41cc
Knative Automation <automation@knative.team>
2022-02-04 01:39:36 +0000

    upgrade to latest dependencies
    bumping knative.dev/eventing 25bf6fe...fb09241:
      > fb09241 tame a bit the duck (# 6130)
      > 7df34f8 Update community files (# 6132)
      > 62b25af :lipstick: fixing the golang path for the migrator to point to actual pkg (# 6133)
      > 1748795 Remove deprecated and unmaintained repos from downstream action (# 6131)
      > cf675de Update community files (# 6115)
      > c959bfb :lipstick: updating to newer versions of k8s for GH Actions :dizzy: (# 6122)
    bumping knative.dev/serving 770b911...fe023e3:
      > fe023e3 Update net-istio nightly (# 12591)
      > d454d67 Update net-contour nightly (# 12584)
      > fc43e7c add script to run performance tests using kperf (# 12549)
      > 62d5aa3 Updates selector labels to `app.kubernetes.io` (# 12587)
      > 997dea3 Update net-kourier nightly (# 12592)
      > 82ad2cd Update net-certmanager nightly (# 12590)
      > 1d95294 Adds a flag to allow customized request headers in conformance tests (# 12540)
      > 792260d upgrade to latest dependencies (# 12583)
      > 0e6c2d8 Update community files (# 12589)
      > 26a2553 Update actions (# 12582)
      > 2d23214 Move maxDurationSeconds tests from conformance tests to e2e tests (# 12526)
      > 4e17128 Add flag disable-test-cleanup (# 10001) (# 12482)
      > 721c2bb upgrade to latest dependencies (# 12575)
      > 5ce87a4 Update net-certmanager nightly (# 12577)
      > 923b4fa Update actions (# 12574)
      > 72ae543 Update community files (# 12573)
      > fb1b4c1 Update net-istio nightly (# 12576)
    bumping knative.dev/hack 82bfca1...9b303d6:
      > 9b303d6 Update community files (# 148)
    bumping knative.dev/networking d23a068...9ce389c:
      > 9ce389c Update community files (# 614)
      > 8ea3045 Update actions (# 613)
      > e266de2 Update actions (# 612)
      > 7002b9a Update community files (# 610)
    bumping knative.dev/pkg f4b57ae...51be315:
      > 51be315 Update community files (# 2419)
      > b619c92 Update actions (# 2417)
      > bab3d0d Update actions (# 2413)
      > 671e46d :dizzy: we must use `flags` instead of `pflags`, since this is not working. It  seems like pflag.* adds the var to its own flag set, not the one package flag uses, and it doesn't expose the internal flag.Var externally - hence this fix. (# 2414)
      > 7880fb4 Update community files (# 2412)
      > 8681fe2 Continual tests logging within test scope (# 2293)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/serving/test/e2e_flags.go
+++ vendor/knative.dev/serving/test/e2e_flags.go
@@ -21,6 +21,8 @@ package test
 
 import (
 	"flag"
+	"net/http"
+	"strings"
 
 	// Load the generic flags of knative.dev/pkg too.
 	_ "knative.dev/pkg/test"
@@ -40,10 +42,12 @@ type ServingEnvironmentFlags struct {
 	EnableBetaFeatures       bool   // Indicates whether we run tests for beta features
 	DisableLogStream         bool   // Indicates whether log streaming is disabled
 	DisableOptionalAPI       bool   // Indicates whether to skip conformance tests against optional API
+	SkipCleanupOnFail        bool   // Indicates whether to skip cleanup if test fails
 	TestNamespace            string // Default namespace for Serving E2E/Conformance tests
 	AltTestNamespace         string // Alternative namespace for running cross-namespace tests in
 	TLSTestNamespace         string // Namespace for Serving TLS tests
 	ExceedingMemoryLimitSize int    // Memory size used to trigger a non-200 response when the service is set with 300MB memory limit.
+	RequestHeaders           string // Extra HTTP request headers sent to the testing deployed KServices.
 }
 
 func initializeServingFlags() *ServingEnvironmentFlags {
@@ -76,6 +80,8 @@ func initializeServingFlags() *ServingEnvironmentFlags {
 	flag.BoolVar(&f.DisableOptionalAPI, "disable-optional-api", false,
 		"Set this flag to skip conformance tests against optional API.")
 
+	flag.BoolVar(&f.SkipCleanupOnFail, "skip-cleanup-on-fail", false, "Set this flag to skip cleanup if test fails.")
+
 	flag.StringVar(&f.TestNamespace, "test-namespace", "serving-tests",
 		"Set this flag to change the default namespace for running tests.")
 
@@ -89,5 +95,27 @@ func initializeServingFlags() *ServingEnvironmentFlags {
 		"Set this flag to the MB of memory consumed by your service in resource limit tests. "+
 			"You service is set with 300 MB memory limit and shoud return a non-200 response when consuming such amount of memory.")
 
+	flag.StringVar(&f.RequestHeaders, "request-headers", "",
+		"Set this flag to add extra HTTP request headers sent to the testing deployed KServices. "+
+			"Format: -request-headers=key1,value1,key2,value2")
+
 	return &f
+}
+
+// RequestHeader returns a http.Header object including key-value header pairs passed via testing flag.
+func (f *ServingEnvironmentFlags) RequestHeader() http.Header {
+	header := make(http.Header)
+
+	if f.RequestHeaders != "" {
+		headers := strings.Split(f.RequestHeaders, ",")
+		if len(headers)%2 != 0 {
+			panic("incorrect input of request headers: " + f.RequestHeaders)
+		}
+
+		for i := 0; i < len(headers); i += 2 {
+			header.Add(headers[i], headers[i+1])
+		}
+	}
+
+	return header
 }
