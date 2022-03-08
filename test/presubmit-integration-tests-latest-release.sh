bcd25e4d9294fb49a4a79b20ba4dcfaf76dc7687
Krsna Mahapatra <kmahapatra@vmware.com>
2022-03-08 15:55:03 -0500

    bump version pointers in integration test

--- test/presubmit-integration-tests-latest-release.sh
+++ test/presubmit-integration-tests-latest-release.sh
@@ -17,7 +17,7 @@
 # This script is used in Knative/test-infra as a custom prow job to run the
 # integration tests against Knative Serving / Eventing of a specific version.
 
-export KNATIVE_SERVING_VERSION="1.2.0"
-export KNATIVE_EVENTING_VERSION="1.2.0"
+export KNATIVE_SERVING_VERSION="1.3.0"
+export KNATIVE_EVENTING_VERSION="1.3.0"
 
 $(dirname $0)/presubmit-tests.sh --integration-tests
