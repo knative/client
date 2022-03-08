c2fe56c83038e302483c8517d8bfcab17cac3542
David Simansky <dsimansk@redhat.com>
2022-03-08 20:51:05 +0100

    Refactor version command to use go modules (#1621)

--- pkg/kn/commands/version/version.go
+++ pkg/kn/commands/version/version.go
@@ -17,11 +17,13 @@ package version
 import (
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
-	"sigs.k8s.io/yaml"
-
+	"golang.org/x/mod/semver"
 	"knative.dev/client/pkg/kn/commands"
+	"sigs.k8s.io/yaml"
 )
 
 var Version string
@@ -29,17 +31,6 @@ var BuildDate string
 var GitRevision string
 var VersionServing string
 var VersionEventing string
-
-// update this var as we add more deps
-var apiVersions = map[string][]string{
-	"serving": {
-		fmt.Sprintf("serving.knative.dev/v1 (knative-serving %s)", VersionServing),
-	},
-	"eventing": {
-		fmt.Sprintf("sources.knative.dev/v1 (knative-eventing %s)", VersionEventing),
-		fmt.Sprintf("eventing.knative.dev/v1 (knative-eventing %s)", VersionEventing),
-	},
-}
 
 type knVersion struct {
 	Version       string
@@ -54,8 +45,41 @@ func NewVersionCommand(p *commands.KnParams) *cobra.Command {
 		Use:   "version",
 		Short: "Show the version of this client",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			info, ok := debug.ReadBuildInfo()
+			var apiVersions map[string][]string
+			if ok {
+				// Retrieve Serving & Eventing go.mod versions, format: v0.y.z
+				for _, dep := range info.Deps {
+					if strings.Contains(dep.Path, "knative.dev/serving") {
+						VersionServing = dep.Version
+					}
+					if strings.Contains(dep.Path, "knative.dev/eventing") {
+						VersionEventing = dep.Version
+					}
+				}
+				// For valid {Version} of kn take MajorMinor + patch version of dependency,
+				// e.g for client's release version=v1.3.0 + serving, eventing modules v0.30.0
+				// the resulting displayed version should be also 'v1.3.0'.
+				// Those versions should represent production releases tagged and published.
+				// Otherwise keep plain unmodified version from go.mod,
+				// that should cover local or nightly builds etc.
+				if semver.IsValid(Version) && semver.IsValid(VersionServing) && semver.IsValid(VersionEventing) {
+					VersionServing = semver.MajorMinor(Version) + VersionServing[5:]
+					VersionEventing = semver.MajorMinor(Version) + VersionEventing[5:]
+				}
+			}
+			apiVersions = map[string][]string{
+				"serving": {
+					fmt.Sprintf("serving.knative.dev/v1 (knative-serving %s)", VersionServing),
+				},
+				"eventing": {
+					fmt.Sprintf("sources.knative.dev/v1 (knative-eventing %s)", VersionEventing),
+					fmt.Sprintf("eventing.knative.dev/v1 (knative-eventing %s)", VersionEventing),
+				},
+			}
+
 			if cmd.Flags().Changed("output") {
-				return printVersionMachineReadable(cmd)
+				return printVersionMachineReadable(cmd, apiVersions)
 			}
 			out := cmd.OutOrStdout()
 			fmt.Fprintf(out, "Version:      %s\n", Version)
@@ -82,7 +106,7 @@ func NewVersionCommand(p *commands.KnParams) *cobra.Command {
 	return versionCmd
 }
 
-func printVersionMachineReadable(cmd *cobra.Command) error {
+func printVersionMachineReadable(cmd *cobra.Command, apiVersions map[string][]string) error {
 	out := cmd.OutOrStdout()
 	v := knVersion{Version, BuildDate, GitRevision, apiVersions}
 	format := cmd.Flag("output").Value.String()
