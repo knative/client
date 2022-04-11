914d60cf9a08698c646ea5ea76bb147e550060af
Gunjan Vyas <vyasgun20@gmail.com>
2022-04-11 18:34:50 +0530

    Fixed timeout help message

--- pkg/kn/commands/service/configuration_edit_flags.go
+++ pkg/kn/commands/service/configuration_edit_flags.go
@@ -220,7 +220,7 @@ func (p *ConfigurationEditFlags) AddCreateFlags(command *cobra.Command) {
 			"any number of times to set multiple labels.")
 	p.markFlagMakesRevision("label")
 	command.Flags().Int64Var(&p.TimeoutSeconds, "timeout", config.DefaultRevisionTimeoutSeconds,
-		"duration in seconds that the request routing layer will wait for a request delivered to a "+""+
+		"Duration in seconds that the request routing layer will wait for a request delivered to a "+""+
 			"container to begin replying")
 	p.markFlagMakesRevision("timeout")
 }
