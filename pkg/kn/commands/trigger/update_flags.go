a9ee31fbb7f6ac2121935e16eb35d09342de3252
cola <45722758+xiangpingjiang@users.noreply.github.com>
2023-08-23 16:15:55 +0800

    Remove deprecated --inject-broker flag from trigger cmds (#1853)

--- pkg/kn/commands/trigger/update_flags.go
+++ pkg/kn/commands/trigger/update_flags.go
@@ -54,12 +54,6 @@ func (f *TriggerUpdateFlags) Add(cmd *cobra.Command) {
 		// Spec.Broker is immutable field
 		cmd.Flags().StringVar(&f.Broker, "broker", "default", "Name of the Broker which the trigger associates with.")
 	}
-	// The Sugar controller was integrated into main Eventing controller. With that the default behavior was changed as well.
-	// Users need to configure 'Automatic Broker Creation' per linked docs.
-	// Deprecated in 1.4, remove in 1.6.
-	cmd.Flags().BoolVar(&f.InjectBroker, "inject-broker", false, "Create new broker with name default through common annotation")
-	cmd.Flags().MarkDeprecated("inject-broker", "effective since 1.4 and will be removed in 1.6 release. \n"+
-		"Please refer to 'Automatic Broker Creation' section for configuration options, "+
-		"https://knative.dev/docs/eventing/sugar/#automatic-broker-creation.")
+
 	cmd.Flags().StringSliceVar(&f.Filters, "filter", nil, "Key-value pair for exact CloudEvent attribute matching against incoming events, e.g type=dev.knative.foo")
 }
