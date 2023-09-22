d92b98db41bdaaf17b7f2763552e22cd80d71418
Knative Automation <automation@knative.team>
2023-09-22 01:38:42 +0000

    upgrade to latest dependencies
    bumping knative.dev/serving 7222bf9...0d73dfe:
      > 0d73dfe Update performance tests (# 14289)
      > d02702e Support gRPC probe (# 14134)
      > 5b1eab3 upgrade to latest dependencies (# 14413)
      > 59ddac3 Update net-contour nightly (# 14412)
      > ecbe40a Update net-certmanager nightly (# 14409)
      > fcb4979 Update net-istio nightly (# 14411)
      > 167d794 Update net-gateway-api nightly (# 14410)
      > 127a351 Cleanup knative-internal-tls secrets (# 14394)
      > d33249b Drop unnecessary `routing-id` in Knative Service certificate (# 14393)
      > 6c602da Update net-contour nightly (# 14405)
      > 7c2a5c6 Update net-certmanager nightly (# 14404)
      > 8818499 Update net-gateway-api nightly (# 14398)
      > 28e75ab Update net-kourier nightly (# 14397)
      > f2fb095 Update net-istio nightly (# 14396)
      > 28effd9 upgrade to latest dependencies (# 14401)
      > 2002b38 test fixture didn't make sense (# 14400)
      > 022f553 Update net-certmanager nightly (# 14362)
      > 50eb65a upgrade to latest dependencies (# 14395)
    bumping knative.dev/eventing 5acf974...ceecbb0:
      > ceecbb0 Subscription allows overriding namespace for Subscriber and Reply (# 7272)
      > 4e7d0f5 [main] Upgrade to latest dependencies (# 7271)
      > 81d8e96 Option to specify different namespace for resources (# 7269)
      > 8a69f32 [main] Upgrade to latest dependencies (# 7267)
      > e407152 Adding description for the built-in event types of our duck sources (# 7263)
      > 401068c [main] Upgrade to latest dependencies (# 7262)
    bumping knative.dev/pkg 7fe699e...8761ceb:
      > 8761ceb upgrade to latest dependencies (# 2827)
    bumping knative.dev/networking 6feaf0c...6aa8805:
      > 6aa8805 Verify and reconcile new keys' value instead of obsleted keys' in secret (# 863)
      > 7f9a4a8 upgrade to latest dependencies (# 862)
    bumping knative.dev/hack 0bb79ff...c4a34c3:
      > c4a34c3 Collect LICENSE files from all dependencies, even without vendor dir (# 316)
      > 38316f2 Upgrade the Go tools to latest ones (# 312)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/hack/library.sh
+++ vendor/knative.dev/hack/library.sh
@@ -497,7 +497,7 @@ function report_go_test() {
   logfile="${logfile/.xml/.jsonl}"
   echo "Running go test with args: ${go_test_args[*]}"
   local gotest_retcode=0
-  go_run gotest.tools/gotestsum@v1.8.0 \
+  go_run gotest.tools/gotestsum@v1.10.1 \
     --format "${GO_TEST_VERBOSITY:-testname}" \
     --junitfile "${xml}" \
     --junitfile-testsuite-name relative \
@@ -510,14 +510,14 @@ function report_go_test() {
   echo "Test log (JSONL) written to ${logfile}"
 
   ansilog="${logfile/.jsonl/-ansi.log}"
-  go_run github.com/haveyoudebuggedit/gotestfmt/v2/cmd/gotestfmt@v2.3.1 \
+  go_run github.com/gotesttools/gotestfmt/v2/cmd/gotestfmt@v2.5.0 \
     -input "${logfile}" \
     -showteststatus \
     -nofail > "$ansilog"
   echo "Test log (ANSI) written to ${ansilog}"
 
   htmllog="${logfile/.jsonl/.html}"
-  go_run github.com/buildkite/terminal-to-html/v3/cmd/terminal-to-html@v3.6.1 \
+  go_run github.com/buildkite/terminal-to-html/v3/cmd/terminal-to-html@v3.9.1 \
     --preview < "$ansilog" > "$htmllog"
   echo "Test log (HTML) written to ${htmllog}"
 
@@ -731,26 +731,22 @@ function __go_update_deps_for_module() {
   fi
   eval "$orig_pipefail_opt"
 
-  if ! [[ "${FORCE_VENDOR:-false}" == "true" ]] && ! [ -d vendor ]; then
-    return 0
+  if [[ "${FORCE_VENDOR:-false}" == "true" ]] || [ -d vendor ]; then
+    group "Removing unwanted vendor files"
+    find vendor/ \( -name "OWNERS" \
+      -o -name "OWNERS_ALIASES" \
+      -o -name "BUILD" \
+      -o -name "BUILD.bazel" \
+      -o -name "*_test.go" \) -exec rm -f {} +
+
+    export GOFLAGS=-mod=vendor
+
+    group "Removing broken symlinks"
+    remove_broken_symlinks ./vendor
   fi
-
-  group "Removing unwanted vendor files"
-
-  # Remove unwanted vendor files
-  find vendor/ \( -name "OWNERS" \
-    -o -name "OWNERS_ALIASES" \
-    -o -name "BUILD" \
-    -o -name "BUILD.bazel" \
-    -o -name "*_test.go" \) -exec rm -f {} +
-
-  export GOFLAGS=-mod=vendor
 
   group "Updating licenses"
   update_licenses third_party/VENDOR-LICENSE "./..."
-
-  group "Removing broken symlinks"
-  remove_broken_symlinks ./vendor
   )
 }
 
@@ -793,7 +789,7 @@ function update_licenses() {
   local dst=$1
   local dir=$2
   shift
-  go_run github.com/google/go-licenses@v1.2.1 \
+  go_run github.com/google/go-licenses@v1.6.0 \
     save "${dir}" --save_path="${dst}" --force || \
     { echo "--- FAIL: go-licenses failed to update licenses"; return 1; }
 }
@@ -801,7 +797,7 @@ function update_licenses() {
 # Run go-licenses to check for forbidden licenses.
 function check_licenses() {
   # Check that we don't have any forbidden licenses.
-  go_run github.com/google/go-licenses@v1.2.1 \
+  go_run github.com/google/go-licenses@v1.6.0 \
     check "${REPO_ROOT_DIR}/..." || \
     { echo "--- FAIL: go-licenses failed the license check"; return 1; }
 }
