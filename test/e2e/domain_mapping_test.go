15a9095292e4c57260cf2dff4abd69920415d30f
cola <45722758+xiangpingjiang@users.noreply.github.com>
2023-11-16 23:43:10 +0800

    fix Flaky E2E test: e2e/TestDomain - create domain with TLS (#1888)
    Signed-off-by: pingjiang <xiangpingjiang1998@gmail.com>

--- test/e2e/domain_mapping_test.go
+++ test/e2e/domain_mapping_test.go
@@ -134,7 +134,7 @@ func domainList(r *test.KnRunResultCollector, domainName string) {
 func domainDescribe(r *test.KnRunResultCollector, domainName string, tls bool) {
 	k := test.NewKubectl(r.KnTest().Kn().Namespace())
 	// Wait for Domain Mapping URL to be populated
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 20; i++ {
 		out, err := k.Run("get", "domainmapping", domainName, "-o=jsonpath='{.status.url}'")
 		assert.NilError(r.T(), err)
 		if len(out) > 0 {
