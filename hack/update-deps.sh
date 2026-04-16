89c2f233d6b4caf97501bd67add9b5b10bf80642
David Simansky <dsimansk@redhat.com>
2026-04-16 16:06:38 +0200

    Fix unknown license in gotest.tools lib (#2191)

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -21,4 +21,9 @@ set -o pipefail
 # shellcheck disable=SC1090
 source "$(go run knative.dev/hack/cmd/script library.sh)"
 
+# Library gotest.tools/v3 which has a short-form Apache 2.0 notice
+# that go-licenses v2 fails to classify.
+export GO_LICENSES_FLAGS="--ignore gotest.tools/v3"
+
+
 go_update_deps "$@"
